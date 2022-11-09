5a55980d1f2294d1298a680acb594368265100a9
Kiomaliz <70612383+Kiomaliz@users.noreply.github.com>
2022-11-09 11:56:51 -0400

    Add files via upload

---
+++ programa2.js
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------------------------------------- //
+
+x = Math.PI/2
+//Math.cos(x)
+a = Math.cos(x/2)**2  //<--- 0.5000000000000001
+let b1 = Math.tan(x)
+let b2 = Math.sin(x)
+let b3 = Math.tan(x)*2
+//b = (b1+b2)/b3
+//c = (a == b)
+//document.write(c)
+if(a == b){
+    document.write("Si es una identidad")
+}
+else{
+    document.write("No es una identidad")
+}
+
